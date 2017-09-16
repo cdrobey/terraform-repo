aa0e80a9cb2a5a76123ae159a366d61ad9947f2a
Chris Roberson <cdroberson@gmail.com>
2017-09-16 09:38:51 -0600

    Building a VMware provider based lab setup.

--- vmware/old/jenkins/provider.tf
+++
@@ -1,8 +0,0 @@
-# Configure the VMware vSphere Provider
-provider "vsphere" {
-  user           = "${var.viuser}"
-  password       = "${var.vipassword}"
-  vsphere_server = "${var.viserver}"
-  # if you have a self-signed cert
-  allow_unverified_ssl = true
-}
