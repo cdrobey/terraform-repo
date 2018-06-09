150ded3719abbd1e61a04255a48ad41d0d19ce6e
Chris Roberson <chris.roberson@puppet.com>
2018-06-09 09:37:39 -0600

    Basic GCP Setup.

--- gcp/provider.tf
+++ gcp/provider.tf
@@ -3,6 +3,6 @@
 #--------------------------------------------------------------
 provider "google" {
   credentials = "${file("${var.gcp_auth_path}")}"
-  project     = "${var.gcp_project}"
-  region      = "${var.gcp_region}"
+  project     = "${var.project}"
+  region      = "${var.region}"
 }
--- gcp/vars.tf
+++ gcp/vars.tf
@@ -1,57 +1,126 @@
 #--------------------------------------------------------------
 # Global Variables
 #--------------------------------------------------------------
-variable aws_access_key     {}
-variable aws_secret_key     {}
-variable aws_sshkey         {}
-variable aws_sshkey_path    {}
+variable gcp_auth_path {}
 
 #--------------------------------------------------------------
 # Site Variables
 #--------------------------------------------------------------
-variable region             { default = "us-west-2" }
-variable vpc                { default = "cdrobey-vpc" }
-variable network0_cidr      { default = "10.1.0.0/16" }
-variable network0_subnet0   { default = "10.1.1.0/24" }
-variable network0_sg        { default = "cdrobey_sg" }
-variable domain             { default = "us-west-2.compute.internal" }
+variable region {
+  default = "us-east1-b"
+}
+
+variable vpc {
+  default = "cdrobey-tf"
+}
+
+variable network0_cidr {
+  default = "10.1.0.0/16"
+}
+
+variable network0_subnet0 {
+  default = "10.1.1.0/24"
+}
+
+variable network0_sg {
+  default = "cdrobey_sg"
+}
+
+variable domain {
+  default = "us-west-2.compute.internal"
+}
 
 #--------------------------------------------------------------
 # Instance Variables
 #--------------------------------------------------------------
-variable pp_role        { default = "base" }
-variable pp_environment { default = "production" }
-variable pp_application { default = "generic" }
+variable pp_role {
+  default = "base"
+}
+
+variable pp_environment {
+  default = "production"
+}
+
+variable pp_application {
+  default = "generic"
+}
 
 #--------------------------------------------------------------
 # Puppet Master Provisioning Variables
 #--------------------------------------------------------------
-variable puppet_name       { default = "cdrobey-puppet" }
-variable puppet_ami        { default = "ami-6f68cf0f" }
-variable git_pri_key       { default = "~/.ssh/github" }
-variable git_pub_key       { default = "~/.ssh/github.pub" }
-variable git_url           { default = "https://github.com/cdrobey/puppet-repo" }
-variable eyaml_pri_key     { default = "~/.eyaml/private_key.pkcs7.pem" }
-variable eyaml_pub_key     { default = "~/.eyaml/public_key.pkcs7.pem" }
+variable puppet_name {
+  default = "cdrobey-puppet"
+}
+
+variable puppet_ami {
+  default = "ami-6f68cf0f"
+}
+
+variable git_pri_key {
+  default = "~/.ssh/github"
+}
+
+variable git_pub_key {
+  default = "~/.ssh/github.pub"
+}
+
+variable git_url {
+  default = "https://github.com/cdrobey/puppet-repo"
+}
+
+variable eyaml_pri_key {
+  default = "~/.eyaml/private_key.pkcs7.pem"
+}
+
+variable eyaml_pub_key {
+  default = "~/.eyaml/public_key.pkcs7.pem"
+}
 
 #--------------------------------------------------------------
 # Jenkins Server Provisioning Variables
 #--------------------------------------------------------------
-variable jenkins_name           { default = "cdrobey-jenkins" }
-variable jenkins_ami            { default = "ami-6f68cf0f" }
-variable jenkins_pp_role        { default = "jenkins"}
-variable jenkins_pp_application { default = "jenkins"}
-variable jenkins_pp_environment { default = "production"}
+variable jenkins_name {
+  default = "cdrobey-jenkins"
+}
+
+variable jenkins_ami {
+  default = "ami-6f68cf0f"
+}
+
+variable jenkins_pp_role {
+  default = "jenkins"
+}
+
+variable jenkins_pp_application {
+  default = "jenkins"
+}
+
+variable jenkins_pp_environment {
+  default = "production"
+}
 
 #--------------------------------------------------------------
 # LINUX Server Provisioning Variables
 #--------------------------------------------------------------
-variable linux_name     { default = "cdrobey-linux" }
-variable linux_ami      { default = "ami-6f68cf0f" }
+variable linux_name {
+  default = "cdrobey-linux"
+}
+
+variable linux_ami {
+  default = "ami-6f68cf0f"
+}
 
 #--------------------------------------------------------------
 # Windows Server Provisioning Variables
 #--------------------------------------------------------------
-variable windows_name     { default = "cdrobey-window" }
-variable windows_ami      { default = "ami-d7a114af" }
-variable windows_password { default = "Adm1nistrat0r" }
+variable windows_name {
+  default = "cdrobey-window"
+}
+
+variable windows_ami {
+  default = "ami-d7a114af"
+}
+
+variable windows_password {
+  default = "Adm1nistrat0r"
+}
